0233da34bc56095155a3eac1560fe5d111dc073c
ABJ135 <1515.abubakarjaved.m2@gmail.com>
2024-11-10 20:22:04 +0500

    data js file for books created

---
+++ data.js
@@ -0,0 +1,22 @@
+const books = [
+    {
+        id : 1,
+        title: 'Book 1',
+        author : 'John',
+        is_available: true
+    },
+    {
+        id : 2,
+        title: 'Book 2',
+        author : 'Anas',
+        is_available: true
+    },{
+        id : 3,
+        title: 'Book 3',
+        author : 'Alice',
+        is_available: true
+    }
+
+]
+
+module.exports = { books };
